15363dd85ece7184d146959fe211831d21b28e35
David Bauer <mail@david-bauer.net>
2018-08-07 23:33:40 +0200

    initial commit

---
+++ compile_uboot.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+BOARD=$1
+BUILDROOT=buildroot-gcc342
+AVAILABLE_BOARDS=$(ls | grep -P '_config$' | sed 's/_config//')
+
+print_board_list () {
+	echo "No valid Board set!"
+	echo "Valid boards:"
+	for B in $AVAILABLE_BOARDS; do
+		echo "* ${B}"
+	done
+
+}
+
+if [ -z "$BOARD" ]; then
+	print_board_list
+	exit 1
+fi
+
+rm .config
+
+if [ ! -d "$BUILDROOT" ]; then
+	tar -xjf ${BUILDROOT}.tar.bz2
+fi
+ln -s ${BOARD}_config .config
+ln -s ${BOARD}_autoconf_h autoconf.h
+FACTORY_UBOOT=n make
